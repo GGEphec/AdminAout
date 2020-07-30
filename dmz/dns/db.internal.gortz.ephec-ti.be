01286092034605fd27cea9b86573ca162e5586f1
GGEphec <g.gortz@students.ephec.be>
2020-07-30 21:12:40 +0200

    Document

---
+++ dmz/dns/db.internal.gortz.ephec-ti.be
@@ -0,0 +1,27 @@
+$ORIGIN	gortz.ephec-ti.be
+$TTL	604800
+
+@	IN	SOA	ns.gortz.ephec-ti.be. (
+			1 ; Serial
+		   604800 ; Refresh
+		    86400 ; Retry
+		  2419200 ; Expire
+		   604800); Negative Cache TTL
+
+; SOA - NS record
+@	IN	NS	ns.gortz.ephec-ti.be.
+
+
+
+; SOA - A record
+ns	IN	A	51.178.41.130
+
+
+; web
+www	IN	A	51.178.41.130
+b2b	IN	A	51.178.41.130
+interne	IN	A	51.178.41.130
+
+
+; voip
+
---
+++ web/b2b/index.php
@@ -0,0 +1,26 @@
+
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>Site pour les revendeurs de woodytoys</h2>
+	<p>Ceci est le site pour les revendeur de woodytoys</p>
+	<p>Il est accessible depuis l extérieur</p>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
